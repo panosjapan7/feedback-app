d866fc1ca315ff90843a67429150315682cbb93b
panosjapan7 <panosjapan@gmail.com>
2022-05-12 09:49:44 +0200

    all files

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import {motion, AnimatePresence} from "framer-motion"
+import FeedbackItem from "./FeedbackItem"
+import FeedbackContext from "../context/FeedbackContext";
+
+function FeedbackList() {
+
+  const {feedback} = useContext(FeedbackContext);
+
+  // console.log(feedback)
+    if(!feedback || feedback.length == 0) {
+        return <p>No Feedback Yet</p>
+    }
+
+  // (version with the animation)
+  return (
+    <div className="feedback-list">
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div
+              key={item.id}
+              initial={{opacity: 0}}
+              animate={{opacity: 1}}
+              exit={{opacity: 0}}
+            >
+                <FeedbackItem
+                  key={item.id}
+                  item={item}
+                  />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+    </div>
+  )
+
+  // (version without the animation)
+  // return (
+  //   <div className="feedback-list">
+  //       {feedback.map((item) => (
+  //           <FeedbackItem
+  //             key={item.id}
+  //             item={item}
+  //             handleDelete={handleDelete}
+  //           />
+  //       ))}
+  //   </div>
+  // )
+}
+
+export default FeedbackList
+
